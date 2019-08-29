8547b117f816f6b901ad6e0334970e406b3c1670
Denis Brun <denis.brun@outlook.com>
2019-08-29 13:24:25 -0300

    Added minor mobile layout fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+// Declaring constants that link to elements in Document
+const runTrainingButton = document.getElementById('runTrainingButton');
+const resultDiv = document.getElementById('output');
+const slopeInput = document.getElementById('slope');
+const offsetInput = document.getElementById('offset');
+
+// Asynchronus function that trains a ML model
 async function run() {
+  resultDiv.className = '';    
+  resultDiv.innerText = `Training...`;
   resultDiv.style.visibility = 'visible';
+  
   // Create a simple model.
   const model = tf.sequential();
   model.add(tf.layers.dense({units: 1, inputShape: [1]}));
@@ -13,40 +23,46 @@ async function run() {
   const xs = tf.tensor2d(array, [length, 1]);
 
   const array2 = array.map(function(item){
-    console.log(typeof(slopeInput.value))
     return (item*Number(slopeInput.value))+(Number(offsetInput.value));
   })
-  console.log(array2);
   
   
   const ys = tf.tensor2d(array2, [length, 1]);
 
   // Train the model using the data.
-  await model.fit(xs, ys, {epochs: 280});
+  await model.fit(xs, ys, {epochs: 100});
 
   // Use the model to do inference on a data point the model hasn't seen.
   // Should print approximately 39.
   resultDiv.innerText = `Using machine learning, the prediction for an input of 20 is ${model.predict(tf.tensor2d([20], [1, 1])).dataSync()}`;
+
 }
 
-const runTrainingButton = document.getElementById('runTrainingButton');
-const resultDiv = document.getElementById('output');
-const slopeInput = document.getElementById('slope');
-const offsetInput = document.getElementById('offset');
 
 runTrainingButton.addEventListener('click', checkIfCanTrain)
 
 function checkIfCanTrain() {
   if(offsetInput.value === '' || slopeInput.value === '') {
-    resultDiv.style.visibility = 'visible';
-    resultDiv.className = 'alert';
-    resultDiv.innerText = `Enter values for slope and offset before training neural network`;
-    console.log("enter data before training");
+    showErrorMessage();
+    setTimeout(clearErrorMessage,3000);
   } else {    
-    resultDiv.className = '';    
-    resultDiv.innerText = `Training...`;
+    
     console.log(`y = ${slopeInput.value}x + ${offsetInput.value}`);
     
     run();
   }
 }
+
+function showErrorMessage() {
+  resultDiv.style.visibility = 'visible';
+  resultDiv.className = 'alert';
+  resultDiv.innerText = `Enter values for slope and offset before training neural network`;
+  // console.log("enter data before training");
+}
+
+function clearErrorMessage() {
+  resultDiv.style.visibility = 'hidden';
+  resultDiv.className = '';  
+}
+
+
