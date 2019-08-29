138ae5bdef0760cca3606d338dd15f0e8bc25dfd
Denis Brun <denis.brun@outlook.com>
2019-08-29 08:42:30 -0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+async function run() {
+  resultDiv.style.visibility = 'visible';
+  // Create a simple model.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+  // Generate some synthetic data for training. (y = 2x - 1)
+  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+
+  const array = [-1, 0, 1, 2, 3, 4];
+  const array2 = array.map(function(item){
+    
+    return (item*slopeInput.value)-offsetInput.value;
+  })
+  console.log(array2);
+  
+  
+  const ys = tf.tensor2d(array2, [6, 1]);
+
+  // Train the model using the data.
+  await model.fit(xs, ys, {epochs: 350});
+
+  // Use the model to do inference on a data point the model hasn't seen.
+  // Should print approximately 39.
+  document.getElementById('micro-out-div').innerText = `Using machine learning, the prediction for an input of 20 is ${model.predict(tf.tensor2d([20], [1, 1])).dataSync()}`;
+}
+
+const runTrainingButton = document.getElementById('runTrainingButton');
+const resultDiv = document.getElementById('micro-out-div');
+const slopeInput = document.getElementById('slope');
+const offsetInput = document.getElementById('offset');
+
+runTrainingButton.addEventListener('click', checkIfCanTrain)
+
+function checkIfCanTrain() {
+  if(offsetInput.value === '' || slopeInput.value === '') {
+    console.log("enter data before training");
+  } else {
+    console.log(`y = ${slopeInput.value}x + ${offsetInput.value}`);
+    
+    run();
+  }
+}
